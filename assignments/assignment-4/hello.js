b507153ef3cb71dc16f74245dd13953ec73d805b
JohnTatlonghari <john4hari@gmail.com>
2025-03-18 17:11:33 -0700

    Add JavaScript assignment files

---
+++ assignments/assignment-4/hello.js
@@ -0,0 +1,10 @@
+// hello.js
+greeting = "JavaScript World";
+
+const createMessage = (name) => {
+    return `Hello, ${name}!`;
+};
+
+document.getElementById("he1").textContent = "HI!!";
+
+console.log(createMessage(greeting));
