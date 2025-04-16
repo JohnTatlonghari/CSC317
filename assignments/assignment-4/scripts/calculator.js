41843480fa6bf84cf453db806b9c6e5ff2d82b11
JohnTatlonghari <john4hari@gmail.com>
2025-04-16 14:35:26 -0700

    I said final submission but since there's new code provided I had to change the scripting file for calculator. Improved button interaction using transform and transition. Added keyboard functions.

--- assignments/assignment-4/scripts/calculator.js
+++ assignments/assignment-4/scripts/calculator.js
@@ -65,12 +65,98 @@ function checkInput(input) {
     return ((input === '/') || (input === '*') ||  (input === '+') || (input === '-') || (input === '.') || (input === '%'));
 }
 
-//The function that % calls. It's not really how it works but for this project it's good enough. 
+//The function that % calls. It's not really how it works though. 
 function takePercent() {
     display1.value = '%(' + display2.value + ')';
     display2.value = (secureEval(display2.value)) / 100;
 }
 
-function displayKey(key) {
-    
-}
+/*
+
+    CODE INTERFACE PROVIDED BY PROFESSOR BELOW. Changed keyMap call of btnId.
+
+*/
+
+function $(id) {
+    return document.getElementById(id);
+  }
+
+document.addEventListener("keydown", function(event) {
+
+    const key = event.key;
+  
+
+    // Perform calculator logic
+  
+    if ("0123456789+-*/.%".includes(key)) {
+  
+      appendToDisplay(key);
+  
+    } else if (key === "Enter") {
+        
+      event.preventDefault(); // this prevents the form from being submitted
+      flashButton('equals');
+      calculate();
+  
+    }
+    else if (key === "Backspace") {
+        flashButton("AC")
+        clearDisplay();
+  
+    }
+
+  
+    // Apply visual feedback
+    function keyMap(key) {
+
+        if ("0123456789".includes(key)) {
+            // In my implementation, id = key for numerical digits.
+            return key;
+        }
+
+        else if("+-*/.%=".includes(key)) {
+
+            switch (key) {
+                case '+':
+                    return 'addition';
+                case '-':
+                    return 'subtraction';
+                case '*':
+                    return 'multiplication';
+                case '/':
+                    return 'division';
+                case '.':
+                    return 'decimal';
+                case '%':
+                    return 'mod';
+                case '=':
+                    return 'equals';
+                
+            }
+
+        }
+
+    }
+
+    const btnId = keyMap(key); // keyMap needs to be defined above
+    if (btnId) {
+      
+        flashButton(btnId); // flashButton needs to be defined. I gave you this already.
+      
+    }
+
+    });
+
+    function flashButton(id) {
+        const button = $(id);
+        if (!button) return;
+        button.classList.add("pressed");
+        setTimeout(() => {
+          button.classList.remove("pressed");
+        }, 100);
+
+      }
+      
+ 
+  
+  
